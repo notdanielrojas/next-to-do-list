2d6972945356f6924e8a6d148f67966375d380b5
Daniel Rojas <notdanielrojas@gmail.com>
2024-11-13 15:55:50 -0300

    Agregado menú hamburguesa y responsividad

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import React from "react";
 import styles from "@/app/styles/nav.module.css";
 import Image from "next/image";
@@ -10,46 +13,60 @@ import Link from "next/link";
 type Props = {};
 
 const Nav = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <div className={styles.navContainer}>
-        <div className={styles.navUserMenu}>
-          <Image
-            src='/profile.jpg'
-            alt='Profile Picture'
-            width={70}
-            height={70}
-            quality={10}
-            className={styles.userAvatar}
-          />
-          <p>Username</p>
+        <div
+          className={`${styles.hamburgerMenu} ${isOpen ? styles.active : ""}`}
+          onClick={toggleMenu}
+          style={{ zIndex: isOpen ? 1 : "initial" }}
+        >
+          <div className={styles.hamburgerBar}></div>
+          <div className={styles.hamburgerBar}></div>
+          <div className={styles.hamburgerBar}></div>
         </div>
-        <div className={styles.addTaskContainer}>
-          <Link href={""}>
-            <button className={styles.navAddTaskButton}>
-              <IoIosAddCircle />
-              Add Task
-            </button>
-          </Link>
-        </div>
-        <div className={styles.navItems}>
-          <Link href={""} className={styles.navItem}>
-            <CiSearch />
+        <nav className={`${styles.navItems} ${isOpen ? styles.open : ""}`}>
+          <div className={styles.navUserMenu}>
+            <Image
+              src='/profile.jpg'
+              alt='Profile Picture'
+              width={70}
+              height={70}
+              quality={10}
+              className={styles.userAvatar}
+            />
+            <p>Username</p>
+          </div>
+          <div className={styles.addTaskContainer}>
+            <Link href={"/addTask"}>
+              <button className={styles.navAddTaskButton}>
+                <IoIosAddCircle className={styles.menuIcon} />
+                Add Task
+              </button>
+            </Link>
+          </div>
+          <Link href={"/search"} className={styles.navItem}>
+            <CiSearch className={styles.menuIcon} />
             Search
           </Link>
-          <Link href={""} className={styles.navItem}>
-            <LuInbox />
+          <Link href={"/inbox"} className={styles.navItem}>
+            <LuInbox className={styles.menuIcon} />
             Inbox
           </Link>
-          <Link href={""} className={styles.navItem}>
-            <IoCalendarNumberOutline />
+          <Link href={"/"} className={styles.navItem}>
+            <IoCalendarNumberOutline className={styles.menuIcon} />
             Today
           </Link>
-          <Link href={""} className={styles.navItem}>
-            <IoCalendarOutline />
+          <Link href={"/upcoming"} className={styles.navItem}>
+            <IoCalendarOutline className={styles.menuIcon} />
             Upcoming
           </Link>
-        </div>
+        </nav>
       </div>
     </>
   );
