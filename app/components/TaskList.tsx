04aaf124dd102b09c1f08872259dedce2dc3fbd2
Daniel Rojas <notdanielrojas@gmail.com>
2025-06-10 17:32:13 -0400

    build

--- app/components/TaskList.tsx
+++ app/components/TaskList.tsx
@@ -1,22 +1,26 @@
+"use client";
+
 import React from "react";
 import TaskItem from "./TaskItem";
 import styles from "../styles/taskList.module.css";
 import { Task } from "@/types/types";
-import useTasks  from "../hooks/useTasks";
+import useTasks from "../hooks/useTasks";
 
 interface TaskListProps {
   tasks: Task[];
 }
 
 const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
-  const { tasks: handledTasks, handleStatusChange } = useTasks(tasks);
+  const { tasks: handledTasks, handleStatusChange, handleEdit } = useTasks(tasks);
 
   return (
     <div className={styles.taskContainer}>
       {handledTasks.length === 0 ? (
         <div>No tasks match your search.</div>
       ) : (
-        handledTasks.map((task) => <TaskItem key={task.id} task={task} onStatusChange={handleStatusChange} />)
+        handledTasks.map((task) => (
+          <TaskItem key={task.id} task={task} onStatusChange={handleStatusChange} onEdit={handleEdit} />
+        ))
       )}
     </div>
   );
